ca98dd44aa23327c5483bb7ae791c3def7e66127
NicolaPiperis <nicolapiperis9999@gmail.com>
2023-12-05 15:41:20 +0100

    salvataggio articoli e stampa con ciclo

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,61 @@ import { useState } from 'react'
 
 
 function App() {
+  
+  const initialFormData = {
+    title : ''
+  };
+  
+  const [formData, setFormData] = useState(initialFormData);
+  const [articleTitleList, setArticleTitleList] = useState([]);
+  
+  function updateFormData(newValue, fieldName) {
+  
+    const newFormData = { ...formData };
+  
+    newFormData[fieldName] = newValue;
+  
+    setFormData(newFormData);
+  
+  }
+
+  function handleFormSubmit(e) {
+    // Evita il refresh della pagina come normalmente farebbe il form
+    e.preventDefault();
+
+    // Aggiungo l'utente alla lista usersList
+    // Aggiorno lo state
+    setArticleTitleList([...articleTitleList, {
+      ...formData,
+      id: crypto.randomUUID(),
+    }]);
+
+    // Resetto il form
+    setFormData(initialFormData);
+  }
 
   return (
     <>
-    <h1>hello world!</h1>
+      <form onSubmit={handleFormSubmit}>  
+
+        <div>
+          <label htmlFor="name_article" className='block font-bold mb-2'>Tile</label>
+          <input type="text" name='title' placeholder="Enter article's title" className="border px-3 py-4 w-full" value={formData.title} onChange={(e) => updateFormData(e.target.value, 'title')}/>
+        </div>
+
+        <button className="mt-2 px-4 py-3 bg-green-300 hover:bg-green-600">Submit</button>
+
+      </form>
+
+      <div>
+        <ul>
+          {articleTitleList.map((article) => (
+            <li key={article.id}>
+              {article.title} - {article.id}
+            </li>
+          ))}
+        </ul>
+      </div>
     </>
   )
 }
